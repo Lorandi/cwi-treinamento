7a59a8a1c3ee4e4f1b9b8274f768e95abfdf957c
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-01-11 17:42:48 -0300

    Incluindo desafio 9 - runas

---
+++ desafio-09-runas/scripts.js
@@ -0,0 +1,45 @@
+//Desafio-Runas por @lokyrech
+
+/*Objetivo:Criar uma função que com base no Array minhasRunas e no Array receitaPedraSecretaDoSol que retorne um Array indicando somente as runas faltantes para criação da Pedra Secreta do Sol.
+ */
+
+ //Variáveis
+
+let minhasRunas = ["Ansuz", "Thurisaz", "Thurisaz", "Thurisaz", "Thurisaz", "Kenaz", "Kenaz", "Kenaz", "Kenaz", "Kenaz", "Kenaz", "Kenaz", "Kenaz", "Gebo", "Wunjo", "Wunjo","Wunjo", "Jara", "Fehu", "Fehu", "Fehu", "Fehu","Fehu", "Fehu", "Fehu", "Fehu", "Raidho", "Raidho", "Mannaz", "Mannaz", "Mannaz"];
+
+let receitaPedraSecretaDoSol = ["Ansuz", "Ansuz", "Elhaz", "Gebo", "Mannaz", "Raidho", "Sowilo", "Tiwaz"];
+
+let runasFaltando = [];
+
+let jaTenho;
+
+let count = 0;
+
+minhasRunas.sort()
+receitaPedraSecretaDoSol.sort()
+
+//Funções
+
+function print(i){ 
+        console.log(i);    
+    }
+
+function verificarRunasFaltando(){
+        for (i = 0; i < receitaPedraSecretaDoSol.length; i++){
+                jaTenho = false;
+                for(j = 0; j < minhasRunas.length; j++){
+                        count++
+                        if(minhasRunas[j] == receitaPedraSecretaDoSol[i] ){
+                                jaTenho = true;                                
+                                break;
+                        }
+                }
+                if(!jaTenho){
+                        runasFaltando.push(receitaPedraSecretaDoSol[i])
+                }
+        }        
+        print(runasFaltando)
+        print(count)
+}
+
+verificarRunasFaltando()
