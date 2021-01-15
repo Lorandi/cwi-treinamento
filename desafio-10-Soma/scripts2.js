85990879724a962b06a44a105ab02cc5f92a1c20
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-01-15 17:23:28 -0300

    Mais algoritimos no desafis 10-soma

---
+++ desafio-10-Soma/scripts2.js
@@ -0,0 +1,31 @@
+
+function somaDigitos(num){
+    soma=0;
+    number = num.toString();
+    if(number.length==1){
+      return soma = parseInt(number.charAt(0));
+    }else{
+      for(i=0;i<number.length;i++){
+      soma += parseInt(number.charAt(i));
+      }
+      if (soma <= 9){
+        return soma;
+      }else{
+        somaDigitos(soma)
+        //  do{
+        //      somaTemp = 0;
+        //      somaString = soma.toString()
+        //      for(i=0; i < somaString.length; i++){                
+        //         somaTemp += parseInt(somaString[i])
+        //     }
+        //     somaString = somaTemp.toString();
+        // }while(somaTemp>9)
+        // soma = somaTemp;                                   
+      }      
+    }
+    return soma;
+  }
+console.log(somaDigitos(2)); //2
+console.log(somaDigitos(2351)); //2
+console.log(somaDigitos(7992312398)) //8
+console.log(somaDigitos(123456789)) //9
